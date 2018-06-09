c87959843ffc29c3bbfc18749ecd79de2635f2eb
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-09 18:44:09 +0000

    added user

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
+var passport = require("passport");
+var bodyParser = require("body-parser");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+var User = require("./models/user");
+
 mongoose.connect("mongodb://localhost/auth_demo_app");
 app.set("view engine", "ejs");
 
