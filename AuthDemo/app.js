d7eb16036ef1c70711e356af92c72ae7f609944f
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-09 18:47:55 +0000

    added passport function

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -6,9 +6,11 @@ var bodyParser = require("body-parser");
 var LocalStrategy = require("passport-local");
 var passportLocalMongoose = require("passport-local-mongoose");
 var User = require("./models/user");
-var User2 = require("./models/user");
+
 mongoose.connect("mongodb://localhost/auth_demo_app");
 app.set("view engine", "ejs");
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.get("/", function(req, res){
