9dfa2180c3340334b4117ff6d999352597dae80d
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-09 19:32:42 +0000

    authenticate added

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -18,6 +18,7 @@ app.set("view engine", "ejs");
 app.use(passport.initialize());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
